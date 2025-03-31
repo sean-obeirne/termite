c47e8f49db7477f4bb5ec6291a98215d952d6aa5
Sean O'Beirne <sean.t.obeirne@gmail.com>
2025-03-30 21:37:04 -0400

    basic cli outline

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use ratatui::*;
 use ratatui::{
     widgets::{Block, Borders, Paragraph},
     layout::{Rect},
+    style::{Style, Modifier, Color},
+    text::{Span},
 };
 
-use crossterm::event::{Event, KeyEvent, KeyEventKind};
+use crossterm::event::{Event, KeyEvent, KeyEventKind, KeyCode};
 
 /* these are just for reference of other things i might want to import
-use crossterm::event::{self, KeyCode, KeyEvent};
+use crossterm::event::{self, KeyEvent};
 use ratatui::{
     buffer::Buffer,
     style::Stylize,
@@ -22,6 +24,9 @@ use ratatui::{
 */
 
 
+const BORDER_COLOR: Color = Color::White;
+const ACTIVE_TITLE_COLOR: Color = Color::Green;
+const INACTIVE_TITLE_COLOR: Color = Color::Blue;
 
 
 fn main() {
@@ -32,12 +37,43 @@ fn main() {
     println!("Returning from ratatui...");
 }
 
-#[derive(Default, Debug)]   // create default constructor and debug printing
+//enum State {
+//    Expanded, // like a typical terminal with some amount of history lines above the input line
+//    Minimal,  // a single input line
+//}
+
+#[derive(Debug)]   // create default constructor and debug printing
 struct App {
-    exit: bool, // should the program exit?
+    exit: bool,             // should the program exit?
+    //state: State,         // how should it look?
+    lines: u16,   // how many history lines are visible?
+    width: u16,
+    margin: u16,
+    title: String,
+    text_buffer: String,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            exit: false,
+            lines: 3,
+            width: 10,
+            margin: 3,
+            title: "Command Line:".to_string(),
+            text_buffer: "".to_string(),
+        }
+    }
 }
 
 impl App {
+
+    fn exit(&mut self) {
+        self.exit = true
+    }
+
+    /////////// RATATUI ////////////
+
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {                              // App loop
             terminal.draw(|frame| self.draw(frame))?;   // draw
@@ -47,16 +83,44 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(Block::default().borders(Borders::ALL), Rect::new(0, 0, 5, 5)); // create a small box with borders
+        let screen_area: Rect = frame.area();
+
+        let borders = match &self.lines {
+            1 => Borders::NONE,
+            2 => Borders::TOP,
+            _ => Borders::ALL,
+        };
+
+        frame.render_widget(
+            Block::default()
+                .borders(borders)
+                .border_style(Style::default().fg(BORDER_COLOR))
+                .title(format!(" {} ", self.title.clone()))
+                .title_style(Style::default().fg(ACTIVE_TITLE_COLOR).remove_modifier(Modifier::BOLD)),
+            Rect::new(
+                self.margin, 
+                screen_area.height - self.lines, 
+                screen_area.width - (2 * self.margin), 
+                self.lines
+            )
+        );
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => { // in enum this is saying, if we have a Key, save as key_event
-                self.exit = true // terminate whenever a key is pressed
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_keypress(key_event)
             }
-            _ => {} // nothing happens otherwise
+            _ => {}
         }
-        Ok(()) // all good!
+        Ok(())
+    }
+
+    fn handle_keypress(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            //KeyCode::Char(c) => 
+            _ => {}
+        };
     }
 }
